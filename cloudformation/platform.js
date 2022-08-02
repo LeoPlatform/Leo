87c8241d92beeae61067d772051a64925491d423
Leif Brockman <lbrockman@MAC-LBROCKMAN.local>
2022-08-02 17:09:17 -0600

    Update platform.js for SSM / SecretsManager Added resources to create AWS::SecretsManager::Secret that contains all bus tables. Added resource to create AWS::SSM:Parameter that points to the name of the bus stack.

--- cloudformation/platform.js
+++ cloudformation/platform.js
@@ -115,6 +115,107 @@ module.exports = {
 				}
 			},
 			DependsOn: ['Auth', 'Bus', 'Cognito']
+		},
+		StackSecret: {
+			Type: 'AWS::SecretsManager::Secret',
+			Properties : {
+				Description : '{Key,Value} map of bus tables',
+				Name : {
+					'Fn::Sub': [
+						'rstreams-${resourceName}',
+						{
+							'resourceName': {
+								'Fn::Select': [
+									'1',
+									{
+										'Fn::Split': [
+											'/',
+											{
+												Ref: 'Bus'
+											}
+										]
+									}
+								]
+							}
+						}
+					]
+				},
+				SecretString : {'Fn::Sub': [
+						'{\"LeoStream\":\"${leoStreamTable}\",\"LeoCron\":\"${leoCronTable}\",\"LeoEvent\":\"${leoEventTable}\",\"LeoSettings\":\"${leoSettingsTable}\",\"LeoSystem\":\"${leoSystemTable}\",\"LeoKinesisStream\":\"${leoKinesisStreamTable}\",\"LeoFirehoseStream\":\"${leoFirehoseStreamTable}\",\"LeoS3\":\"${leoS3Table}\",\"Region\":\"${AWS::Region}\"}',
+						{ 
+							'leoStreamTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoStream'
+								]
+							},
+							'leoCronTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoCron'
+								]
+							},
+							'leoEventTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoEvent'
+								]
+							},
+							'leoSettingsTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoSettings'
+								]
+							},
+							'leoSystemTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoSystem'
+								]
+							},
+							'leoKinesisStreamTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoKinesisStream'
+								]
+							},
+							'leoFirehoseStreamTable': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoFirehoseStream'
+								]
+							},
+							'leoS3Table': {
+								'Fn::GetAtt': [
+									'Bus',
+									'Outputs.LeoS3'
+								]
+							}
+					}
+				]
+				}
+			}
+		},
+		RSFParameter: {
+			Type : 'AWS::SSM::Parameter',
+			Properties : {
+				Description : 'String',
+				Name : {Ref: 'AWS::StackName'},
+				Type : 'String',
+				Value : {
+					'Fn::Select': [
+						'1',
+						{
+							'Fn::Split': [
+								'/',
+								{
+									Ref: 'Bus'
+								}
+							]
+						}
+					]
+				}
+			}
 		}
 	}
 };
