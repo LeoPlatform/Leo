76f30100a39b55bc9d42aec5af6105fdd8ee2270
Arturo Valery <avalery@commercehub.com>
2022-12-22 15:08:09 -0500

    fix: adding type docs and a couple missing params

--- cdk-lib/src/types.ts
+++ cdk-lib/src/types.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 export interface LeoPlatformProps {
+  /**
+   * CFN base parameters
+   */
   readonly baseParameters: BaseParameters;
+  /**
+   * CFN parameter groups
+   */
   readonly parameterGroups: LeoParameterGroups;
 
   /**
@@ -68,77 +74,229 @@ export interface BaseParameters {
    * @default 1
    */
   readonly KinesisShards?: number;
+
+  /**
+   * Number of seconds before LeoStream records are auto deleted
+   * min: 0
+   * @default 0
+   */
+   readonly MonitorShardHashKey?: number;
+
+  /**
+   * Number of seconds before LeoStream records are auto deleted
+   * min: 1
+   * @default 604800
+   */
+  readonly StreamTTLSeconds?: number;
 }
 
 export interface LeoParameterGroups {
+  /**
+   * Lambda props
+   */
   readonly lambdaProps: LambdaProps;
 
+  /**
+   * Leo stream props
+   */
   readonly leoStreamProps: LeoStreamProps;
 
+  /**
+   * Leo archive props
+   */
   readonly leoArchiveProps: LeoArchiveProps;
 
+  /**
+   * Leo event props
+   */
   readonly leoEventProps: LeoEventProps;
 
+  /**
+   * Leo settings props
+   */
   readonly leoSettingsProps: LeoSettingsProps;
 
+  /**
+   * 
+   */
   readonly leoCronProps: LeoCronProps;
 
   readonly leoSystemProps: LeoSystemProps;
 }
 
 export interface LambdaProps {
+  /**
+   * Kinesis stream processor memory
+   * min: 128
+   * max: 10240
+   * @default 640
+   */
   readonly KinesisStreamProcessorMemory?: number;
+  /**
+   * Firehose processor memory
+   * min: 128
+   * max: 10240
+   * @default 640
+   */
   readonly FirehoseStreamProcessorMemory?: number;
+  /**
+   * Cron processor memory
+   * min: 128
+   * max: 10240
+   * @default 256
+   */
   readonly CronProcessorMemory?: number;
+  /**
+   * Event trigger memory
+   * min: 128
+   * max: 10240
+   * @default 128
+   */
   readonly EventTriggerMemory?: number;
+  /**
+   * Leo monitor memory
+   * min: 128
+   * max: 10240
+   * @default 256
+   */
   readonly LeoMonitorMemory?: number;
 }
 
 export interface LeoStreamProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoStreamBillingMode?: BillingModes;
+  /**
+   * @default 20
+   */
   readonly LeoStreamMinReadCapacity?: number;
+  /**
+   * @default 1000
+   */
   readonly LeoStreamMaxReadCapacity?: number;
+  /**
+   * @default 20
+   */
   readonly LeoStreamMinWriteCapacity?: number;
+  /**
+   * @default 1000
+   */
   readonly LeoStreamMaxWriteCapacity?: number;
 }
 
 export interface LeoArchiveProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoArchiveBillingMode?: number;
+  /**
+   * @default 5
+   */
   readonly LeoArchiveMinReadCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoArchiveMaxReadCapacity?: number;
+  /**
+   * @default 5
+   */
   readonly LeoArchiveMinWriteCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoArchiveMaxWriteCapacity?: number;
 }
 
 export interface LeoEventProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoEventBillingMode?: BillingModes;
+  /**
+   * @default 5
+   */
   readonly LeoEventMinReadCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoEventMaxReadCapacity?: number;
+  /**
+   * @default 5
+   */
   readonly LeoEventMinWriteCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoEventMaxWriteCapacity?: number;
 }
 
 export interface LeoSettingsProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoSettingsBillingMode?: BillingModes;
+  /**
+   * @default 5
+   */
   readonly LeoSettingsMinReadCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoSettingsMaxReadCapacity?: number;
+  /**
+   * @default 5
+   */
   readonly LeoSettingsMinWriteCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoSettingsMaxWriteCapacity?: number;
 }
 
 export interface LeoCronProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoCronBillingMode?: BillingModes;
+  /**
+   * @default 20
+   */
   readonly LeoCronMinReadCapacity?: number;
+  /**
+   * @default 200
+   */
   readonly LeoCronMaxReadCapacity?: number;
+  /**
+   * @default 20
+   */
   readonly LeoCronMinWriteCapacity?: number;
+  /**
+   * @default 200
+   */
   readonly LeoCronMaxWriteCapacity?: number;
 }
 
 export interface LeoSystemProps {
+  /**
+   * @default BillingModes.PROVISIONED
+   */
   readonly LeoSystemBillingMode?: BillingModes;
+  /**
+   * @default 5
+   */
   readonly LeoSystemMinReadCapacity?: number;
+  /**
+   * @default 50
+   */
   readonly LeoSystemMaxReadCapacity?: number;
+  /**
+   * @default 5 
+   */
   readonly LeoSystemMinWriteCapacity?: number;
+  /**
+   * @default 50 
+   */
   readonly LeoSystemMaxWriteCapacity?: number;
 }
 
